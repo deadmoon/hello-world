6a2bcb21d94b87fde380e485921f8b593598de9a
Burak Yilmaz <burak.yilmaz@medtronic.com>
2016-05-16 07:45:27 -0400

    Vectors !! :)

--- test.cpp
+++ test.cpp
@@ -1,3 +1,20 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <string>
+
+// Program to read the numbers from a file and find the median: 
 
 
-// This is the test .c
+int main()
+{
+
+    std::vector<double> numbers(4,10); 
+    std::cout<<"The size of the vector is: "<<numbers.size()<<std::endl; 
+    typedef std::vector<double>::size_type vec_sz; 
+    for(int i=0;i<numbers.size();i++) std::cout<<numbers.at(i)<<std::endl;     
+    
+    std::cout<<sizeof(vec_sz); 
+	return 0; 
+
+}
