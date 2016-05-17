d6cddc2d4ed1e5bf3c782f2f57d7292422b19511
Burak Yilmaz <burak.yilmaz@medtronic.com>
2016-05-17 08:01:45 -0400

    const vectors

--- test.cpp
+++ test.cpp
@@ -5,16 +5,25 @@
 
 // Program to read the numbers from a file and find the median: 
 
+using namespace std;
+
 
 int main()
 {
+    double myinit[] = {3,5,7,11};
+    std::vector<double> numbers(myinit,myinit + sizeof(myinit)/sizeof(double)); 
 
-    std::vector<double> numbers(4,10); 
     std::cout<<"The size of the vector is: "<<numbers.size()<<std::endl; 
-    typedef std::vector<double>::size_type vec_sz; 
-    for(int i=0;i<numbers.size();i++) std::cout<<numbers.at(i)<<std::endl;     
+
+    std::vector<double>::size_type size;
     
-    std::cout<<sizeof(vec_sz); 
+    const vector<double>& nm2 = numbers;
+
+    numbers.push_back(40);
+
+    for(int i=0;i<numbers.size();i++) std::cout<<numbers.at(i)<<std::endl;     
+    cout<<std::endl;     
+    for(int i=0;i<nm2.size();i++) std::cout<<nm2.at(i)<<std::endl;     
 	return 0; 
 
 }
